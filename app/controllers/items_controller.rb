78265a4d204ae67f658ae62c2d091bb85808fbe5
kanako-s2222 <hina_haru5121@yahoo.co.jp>
2024-01-22 19:11:57 +0900

    修正4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,8 +14,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.valid?
-      @item.save
+    if @item.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
